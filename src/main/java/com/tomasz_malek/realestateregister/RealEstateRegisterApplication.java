a3df81f00f50db4e1905cb69d95e8d469bd6ea85
Tomasz Małek <tomasz_malek@icloud.com>
2023-10-09 16:55:34 +0200

    first commit

---
+++ src/main/java/com/tomasz_malek/realestateregister/RealEstateRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.tomasz_malek.realestateregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomasz_malek/realestateregister/RealEstateRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomasz_malek.realestateregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
